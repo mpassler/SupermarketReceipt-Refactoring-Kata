205f92fbfa57b394fc21683e89fe61a34787dbf4
michael.passler <michael.passler@andrena.de>
2024-04-25 11:41:22 +0200

    remove redundant else

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -64,24 +64,23 @@ public class ShoppingCart {
                     if (discount != null)
                         receipt.addDiscount(discount);
                     continue;
-                } else {
-                    Discount discount = null;
-                    int x = 1;
-                    if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT) {
-                        x = 5;
-                    }
-                    int numberOfXs = quantityAsInt / x;
-                    if (offer.offerType == SpecialOfferType.TEN_PERCENT_DISCOUNT) {
-                        discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
-                    }
-                    if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT && quantityAsInt >= 5) {
-                        double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                        discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
-                    }
-                    if (discount != null)
-                        receipt.addDiscount(discount);
-                    continue;
                 }
+                Discount discount = null;
+                int x = 1;
+                if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT) {
+                    x = 5;
+                }
+                int numberOfXs = quantityAsInt / x;
+                if (offer.offerType == SpecialOfferType.TEN_PERCENT_DISCOUNT) {
+                    discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
+                }
+                if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT && quantityAsInt >= 5) {
+                    double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                    discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
+                }
+                if (discount != null)
+                    receipt.addDiscount(discount);
+                continue;
             }
         }
     }
