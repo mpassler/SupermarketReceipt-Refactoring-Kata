9a3e1f317d21b2486e9032fb52fd63fe22675fee
michael.passler <michael.passler@andrena.de>
2024-04-25 11:30:18 +0200

    remove else because we continue

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -47,35 +47,34 @@ public class ShoppingCart {
                     if (discount != null)
                         receipt.addDiscount(discount);
                     continue;
-                } else {
-                    Discount discount = null;
-                    int x = 1;
-                    if (offer.offerType == SpecialOfferType.TWO_FOR_AMOUNT) {
-                        x = 2;
-                        if (quantityAsInt >= 2) {
-                            int intDivision = quantityAsInt / x;
-                            double pricePerUnit = offer.argument * intDivision;
-                            double theTotal = (quantityAsInt % 2) * unitPrice;
-                            double total = pricePerUnit + theTotal;
-                            double discountN = unitPrice * quantity - total;
-                            discount = new Discount(p, "2 for " + offer.argument, -discountN);
-                        }
-                    }
-                    if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT) {
-                        x = 5;
-                    }
-                    int numberOfXs = quantityAsInt / x;
-                    if (offer.offerType == SpecialOfferType.TEN_PERCENT_DISCOUNT) {
-                        discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
-                    }
-                    if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT && quantityAsInt >= 5) {
-                        double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                        discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
-                    }
-                    if (discount != null)
-                        receipt.addDiscount(discount);
-                    continue;
                 }
+                Discount discount = null;
+                int x = 1;
+                if (offer.offerType == SpecialOfferType.TWO_FOR_AMOUNT) {
+                    x = 2;
+                    if (quantityAsInt >= 2) {
+                        int intDivision = quantityAsInt / x;
+                        double pricePerUnit = offer.argument * intDivision;
+                        double theTotal = (quantityAsInt % 2) * unitPrice;
+                        double total = pricePerUnit + theTotal;
+                        double discountN = unitPrice * quantity - total;
+                        discount = new Discount(p, "2 for " + offer.argument, -discountN);
+                    }
+                }
+                if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT) {
+                    x = 5;
+                }
+                int numberOfXs = quantityAsInt / x;
+                if (offer.offerType == SpecialOfferType.TEN_PERCENT_DISCOUNT) {
+                    discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
+                }
+                if (offer.offerType == SpecialOfferType.FIVE_FOR_AMOUNT && quantityAsInt >= 5) {
+                    double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                    discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
+                }
+                if (discount != null)
+                    receipt.addDiscount(discount);
+                continue;
             }
         }
     }
