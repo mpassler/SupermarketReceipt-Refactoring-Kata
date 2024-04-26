b7f002b09e0d509ff569306349899d08b1ed983b
michael.passler <michael.passler@andrena.de>
2024-04-26 16:40:45 +0200

    simplify Map
    use Product directly instead of product name

--- java/src/test/java/dojo/supermarket/model/FakeCatalog.java
+++ java/src/test/java/dojo/supermarket/model/FakeCatalog.java
@@ -4,17 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FakeCatalog implements SupermarketCatalog {
-    private Map<String, Product> products = new HashMap<>();
-    private Map<String, Double> prices = new HashMap<>();
+    private final Map<Product, Double> prices = new HashMap<>();
 
     @Override
     public void addProduct(Product product, double price) {
-        this.products.put(product.name(), product);
-        this.prices.put(product.name(), price);
+        this.prices.put(product, price);
     }
 
     @Override
     public double getUnitPrice(Product p) {
-        return this.prices.get(p.name());
+        return this.prices.get(p);
     }
 }
